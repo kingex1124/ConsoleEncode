2f49dfbe92f7c82cbc6a956df6b78da98fd6f5be
kevan.chen <kevan.chen@sinopa.com>
2022-01-27 13:27:27 +0800

    TEST

---
+++ ConsoleEncode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEncode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ASCII碼表 包含 英文 數字 符號 總共255碼
+            // char = unicode 且包含 ASCII
+
+
+            string str = "我是誰";
+
+            byte[] bytes = Encoding.ASCII.GetBytes(str);
+
+            string someString = Encoding.ASCII.GetString(bytes);
+        }
+    }
+}
